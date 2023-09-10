8cad1cdf8099da6d31f3344cda5c223aed9311c2
mikhailrezchikov <mrezchikov529@gmail.com>
2023-09-10 13:25:09 +0300

    added tests

---
+++ src/test/java/tests/SearchTests.java
@@ -0,0 +1,46 @@
+package tests;
+
+
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+public class SearchTests extends TestBase {
+
+
+    @Test
+    @Tag("android")
+    void successfulSearchTest() {
+        step("Ввод поискового запроса", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Проверка результатов поиска", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/search_container"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @Tag("android")
+    void checkErrorTest() {
+        step("Ввод поискового запроса", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+
+        step("Переход на страницу Appium", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container")).first().click();
+        });
+
+        step("Проверка страницы", () -> {
+        $(AppiumBy.id("org.wikipedia.alpha:id/view_wiki_error_button")).shouldBe(visible);
+        $(AppiumBy.id("org.wikipedia.alpha:id/view_wiki_error_text")).shouldHave(text("An error occurred"));
+        });
+    }
+}
